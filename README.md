8a9dd28574ea6f8709b1c9fc30e24667dd493f2e
richardle17 <richardle17@yahoo.com>
2020-08-22 21:00:55 -0700

    added bot functions
    country, state, county

--- README.md
+++ README.md
@@ -5,4 +5,29 @@
 [![PyPI pyversions](https://camo.githubusercontent.com/fd8c489427511a31795637b3168c0d06532f4483/68747470733a2f2f696d672e736869656c64732e696f2f707970692f707976657273696f6e732f77696b6970656469612d6170692e7376673f7374796c653d666c6174)](https://pypi.python.org/pypi/ansicolortags/)
 
 
- Slack bot designed to provide COVID19 statistics 
+Slack bot designed to provide COVID19 statistics 
+
+Uses API from: https://disease.sh/docs/#/
+
+# Requirements
+Python3, Slack Bot API, ngrok localhost webhook.
+
+Use environment variables for API keys:
+
+* SLACK_SIGNING_SECRET
+* slack_token
+* VERIFICATION_TOKEN
+
+# Bot Uses
+
+Retrieves Data from disease.sh API which is based on Worldometers and John Hopkins University API.
+
+* For particular countries, simply @ the bot followed by the country name (case sensitive).
+* For particular states, simply @ the bot followed by the state name (case sensitive).
+* For particular counties, simply @ the bot followed by the word 'county' and the county name (case sensitive).
+
+Todo:
+
+* clean up the code
+* possibly change state and country commands to be based on a keyword (e.g state & country)
+* Make the visual message slack response more fancy?
--- main.py
+++ main.py
@@ -1,12 +1,100 @@
 import json
 import requests
 import os
-import prettytable
 import errno
 from datetime import datetime
+from flask import Flask, Response
+from slackeventsapi import SlackEventAdapter
+from threading import Thread
+from slack import WebClient
+
+# This `app` represents your existing Flask app
+app = Flask(__name__)
+
+# greetings = ["hello", "hello there", "hey"]
+states = ["alaska", "alabama", "arkansas", "american samoa", "arizona", "california", "colorado", "connecticut",
+          "district ", "of columbia", "delaware", "florida", "georgia", "guam", "hawaii", "iowa", "idaho", "illinois",
+          "indiana", "kansas", "kentucky", "louisiana", "massachusetts", "maryland", "maine", "michigan", "minnesota",
+          "missouri", "mississippi", "montana", "north carolina", "north dakota", "nebraska", "new hampshire",
+          "new jersey", "new mexico", "nevada", "new york", "ohio", "oklahoma", "oregon", "pennsylvania", "puerto rico",
+          "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "virginia", "virgin islands",
+          "vermont", "washington", "wisconsin", "west Virginia", "wyoming"]
+countries = ['Germany', 'France', 'China', 'United States']
+help_command = ['help']
 
 
-def get_query_data(state):
+SLACK_SIGNING_SECRET = ''
+slack_token = ''
+VERIFICATION_TOKEN = ''
+
+# instantiating slack client
+slack_client = WebClient(slack_token)
+
+
+# An example of one of your Flask app's routes
+@app.route("/")
+def event_hook(request):
+    json_dict = json.loads(request.body.decode("utf-8"))
+    if json_dict["token"] != VERIFICATION_TOKEN:
+        return {"status": 403}
+
+    if "type" in json_dict:
+        if json_dict["type"] == "url_verification":
+            response_dict = {"challenge": json_dict["challenge"]}
+            return response_dict
+    return {"status": 500}
+    return
+
+
+slack_events_adapter = SlackEventAdapter(
+    SLACK_SIGNING_SECRET, "/slack/events", app
+)
+
+
+@slack_events_adapter.on("app_mention")
+def handle_message(event_data):
+    def send_reply(value):
+        event_data = value
+        message = event_data["event"]
+        if message.get("subtype") is None:
+            command = message.get("text")
+            channel_id = message["channel"]
+            if any(item in command.lower() for item in help_command):
+                message = "@ me with any country, statefor detailed information. For specific counties, use 'county <county>'."
+                slack_client.chat_postMessage(channel=channel_id, text=message)
+            elif any(state in command.lower() for state in states):  # US states
+                my_state = (command.split('>')[1].lstrip().title())
+                response_data = get_state_stats(my_state)
+                message = "State: {} --- Cases: {} --- Deaths: {} --- Active: {}" \
+                    .format(response_data['state'], response_data['cases'], response_data['deaths'],
+                            response_data['active'])
+                slack_client.chat_postMessage(channel=channel_id, text=message)
+            elif any(country in command for country in countries):
+                my_country = (command.split('>')[1].lstrip().title())
+                response_data = get_country_stats(my_country)
+                message = "Country: {} --- Cases: {} --- Deaths: {} --- Active: {}"\
+                    .format(response_data['country'], response_data['cases'], response_data['deaths'], response_data['active'])
+                slack_client.chat_postMessage(channel=channel_id, text=message)
+            elif command.split('> ')[1].split(' ')[0] == 'county':
+                # <> county Santa Clara
+                my_county = (command.split('county ')[1])
+                print('county command: -{}-'.format(my_county))
+                response_data = get_county_stats(my_county)
+                print(response_data)
+                message = "County: {} --- Cases: {} --- Deaths: {} --- Recovered: {}" \
+                    .format(response_data['county'], response_data['cases'], response_data['deaths'], response_data['recovered'])
+                slack_client.chat_postMessage(channel=channel_id, text=message)
+            else:
+                print('unknown command')
+                message = 'Unknown command'
+                slack_client.chat_postMessage(channel=channel_id, text=message)
+
+    thread = Thread(target=send_reply, kwargs={"value": event_data})
+    thread.start()
+    return Response(status=200)
+
+
+def get_state_stats(state):
     query = 'https://disease.sh/v3/covid-19/states?sort=cases&yesterday=false'
     full_data = (requests.get(query))
     if full_data.status_code == 200:
@@ -16,15 +104,31 @@ def get_query_data(state):
         counter = 0
         for num in json_data:
             if num['state'] == state:
-                return json_data[counter]
+                response = json_data[counter]
             counter = counter + 1
+        print(response)
+        return {'state': state, 'cases': response['cases'], 'deaths': response['deaths'], 'active': response['active']}
 
 
+def get_country_stats(country):
+    query = 'https://disease.sh/v3/covid-19/countries/{}?yesterday=true&strict=true'.format(country)
+    full_data = (requests.get(query))
+    if full_data.status_code == 200:
+        json_data = json.loads(full_data.text)
+        return {'country': country, 'cases': json_data['cases'], 'deaths': json_data['deaths'],
+                'active': json_data['active']}
+
+
+def get_county_stats(county):
+    query = 'https://disease.sh/v3/covid-19/jhucsse/counties/{}'.format(county)
+    full_data = (requests.get(query))
+    if full_data.status_code == 200:
+        json_data = json.loads(full_data.text)
+        return {'county': county, 'cases': json_data[0]['stats']['confirmed'], 'deaths': json_data[0]['stats']['deaths'],
+                'recovered': json_data[0]['stats']['recovered']}
+
+
+# Start the server on port 3000
 if __name__ == "__main__":
-    state = 'California'
-    data = get_query_data(state)
-    print(data)
-    output_table = prettytable.PrettyTable([state, ' ', '  '])
-    output_table.add_row(['cases', 'deaths', 'active'])
-    output_table.add_row([data['cases'], data['deaths'], data['active']])
-    print (output_table)
+    app.run(port=3000)
+
